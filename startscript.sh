f8e7974a9fd6040689d7d167226339dfea5e7fb0
Worawut <tonmanna@gmail.com>
2018-03-28 17:30:13 +0700

    Build Only

--- startscript.sh
+++ startscript.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ "$dev" = true ] ; then
-    yarn run watch & yarn start
-else
-    yarn run build:dynamicbundles
-    cd node-api
-    node index.js
-fi
+yarn run build:dynamicbundles
+cd node-api
+node index.js
