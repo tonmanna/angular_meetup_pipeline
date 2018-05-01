281f6f6c250222167e9badf337e9d8cf48d37262
Worawut <tonmanna@gmail.com>
2018-05-01 14:19:52 +0700

    day of meetup

--- kubernetes_cmd/rollingupdate.sh
+++ kubernetes_cmd/rollingupdate.sh
@@ -1,1 +1,1 @@
-kubectl rolling-update anuglarmeetup --image=gcr.io/clean-silo-630/github-tonmanna-angular_meetup_pipeline:ba5b1264921461375e3c6ed509b0c4e16a629249 --image-pull-policy=Always
+kubectl rolling-update anuglarmeetup --image=gcr.io/clean-silo-630/github-tonmanna-angular_meetup_pipeline:bb402450e13c576e277cd40bbaae2a1c1dae6f9a --image-pull-policy=Always
